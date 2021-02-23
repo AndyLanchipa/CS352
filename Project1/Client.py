1f997ebeee196bc3fe8c3234e543bcb4df164daf
Ben R <bwrocco@gmail.com>
2021-02-23 14:59:33 -0500

    Client Update

--- Project1/Client.py
+++ Project1/Client.py
@@ -9,10 +9,23 @@ try:
 except socket.error as err:
     print("socket error: ")
     exit()
-server_addr = (argv[1],argv[2]) #connect to rs listen port since we hae the host name and port number
+server_addr = (argv[1],argv[2]) #connect to rs listen port since we have the host name and port number
 client_sock.connect(server_addr)
+
+message = input(" -> ")
 
 Disconnect = False #this will be true once we send a disconnect message to other sockets
 
-while Disconnect != True:
+while message:
     print("here")
+    response = client_sock.recv(512)
+    response = response.decode('utf-8')
+    lastChar = response[-1]
+
+    if lastChar == "A":
+        response = response[:-1]
+        for line in response:
+            words = line.split(" ")
+            print(words)
+    #else:
+
